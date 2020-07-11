eb1b554ca6963249c4d4e7231fe660f5679178db
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-07-11 17:32:59 +0800

    Add files via upload

---
+++ reactServerRenderer.js
@@ -0,0 +1,29 @@
+// 2020. 11/7, am 5:25
+// DOM is not support in Server, due to location not exist Server.
+// only support Serializable data.
+// Sync & BlockIO
+
+var React = require("react");
+
+var KsComponent = React.createClass({
+
+    var s = React.renderToString(<KsComponent/>);
+
+    render: function(){
+
+        return <div> 前端傳回給後端的畫面 </div>;
+    }
+
+});
+
+// 傳回給後端的畫面
+// 資料格式變成如下
+// 
+
+<div
+    data-reactid = ".doklhpds"
+    data-react-checksum = "-8904225640"
+
+>
+    前端傳回給後端的畫面
+</div>
